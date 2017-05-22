1f4956a4ea10de15e01999f30b34cfcf2429c97c
HydraOrc <hydra0@bigmir.net>
2017-05-22 14:02:05 +0300

    Update translit package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'hydraorc:s3-put-signed-url',
   summary: 'Meteor method that helps you to generate a signed url to PUT a file to amazon aws s3',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/hydraorc/meteor-s3-put-signed-url.git'
 });
 
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
   api.use([
     'alanning:roles@1.2.16',
     'stevezhu:lodash@4.17.2',
-    'hydraorc:translit@2.0.1'
+    'hydraorc:translit@2.0.2'
   ], 'server');
 
   api.addFiles([
